f787ab94c9ccae29abcad5600464386ad6342a33
mustafa1582002 <mustafa1582002@gmail.com>
2023-06-09 15:50:30 +0300

    Add project files.

---
+++ BankSys/Models/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Customer> Customers;
+        public DbSet<TransferProcess> TransferProcesses;
+    }
+}
---
+++ BankSys/Models/TransferProcess.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Models
+{
+    public class TransferProcess
+    {
+        public int Id { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public Double Quantity { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
