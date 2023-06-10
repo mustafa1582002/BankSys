bf1486e28d79588e4aa160146171d46b210ac685
mustafa1582002 <mustafa1582002@gmail.com>
2023-06-10 14:19:36 +0300

    add GIt method

---
+++ BankSys/Controllers/CustomersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankSys.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+
+        public CustomersController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("viewAll")]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var customer =await _context.Customers.ToListAsync(); 
+            return Ok(customer);
+        }
+
+    }
+}
--- BankSys/Models/TransferProcess.cs
+++ BankSys/Models/TransferProcess.cs
@@ -1,10 +1,12 @@
-﻿namespace WebApplication1.Models
+﻿
+namespace WebApplication1.Models
 {
     public class TransferProcess
     {
         public int Id { get; set; }
         public string From { get; set; }
         public string To { get; set; }
+        [MaxLength(100)]
         public Double Quantity { get; set; }
         public int CustomerId { get; set; }
         public Customer Customer { get; set; }
